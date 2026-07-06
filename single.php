08b0138696343c525fe2add549abd36952485c45
developer <developer@developer.com>
2026-07-07 00:23:45 +0100

    feat(single post): created single.php post page and styled it

---
+++ single.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+
+<section class="post-page">
+    <div class="post">
+        <div class="breadcrumb">
+            <a class="breadcrumb__link" href="#">home</a> /
+            <a class="breadcrumb__link" href="#">News</a> /
+            <a class="breadcrumb__link" href="#">Gta 6</a>
+        </div>
+        <div class="container--vetical">
+            <h1 class="post__title">Xbox Series X/S is getting a major price increase, as Microsoft becomes the latest company to blame skyrocketing RAM and storage costs</h1>
+            <p class="post__excerpt">“Is also waving goodbye to 2TB model.”</p>
+        </div>
+    
+    
+        <div class="container--vertical">
+            <a href="#"><img class="card__img" src="<?php echo get_theme_file_uri(); ?>/assets/images/games_god-of-war.jpg" alt=""></a>
+            <p class="post__image-credit">Image Credit : Eurogamer</p>
+        </div>
+    
+        <!-- Author -->
+        <article class="author--horizontal">
+            <a class="author__link" href="#"><img class="author__image" src="<?php echo get_theme_file_uri(); ?>/assets/images/author-image-ai-generated-by-me.webp" alt=""></a>
+            <div class="container--vertical">
+                <div>
+                    <p class="author__full-name " style="text-align:left"><a class="author__link" href="#">Alberto Jalapino</a></p>
+                    <p class="author__role" style="text-align:left">Associate Editor</p>
+                    <p class="author__publishings" style="text-align:left">Published : <span class="author__publishings-nums">150</span> articles</p>
+                </div>
+            </div>
+        </article>
+    
+        <!-- Post Content -->
+         <p class="post__content">Let's start with the good news: Switch 2 has become the second fastest-selling console in US history, after just 12 months on sale. It shifted a whopping 5.9m units, placing it only behind Nintendo's own Game Boy Advance - which sold 6.5m units in the 12 months after it arrived, 25 years ago.
+    
+    These numbers come from data-tracking company Circana's May 2026 report, which also declared Switch 2 to be May's best-selling console in the US, ahead of PlayStation 5. It's also the primary reason why year-on-year hardware spend has risen by more than a third. Otherwise, the news is dire.
+    
+    Onto the bad news, then. PlayStation 5 sales were down 58 percent year-on-year "driven by recent price increases", and fell to their lowest May total since 2000. Meanwhile, Xbox sales fell only 12 percent, which doesn't sound bad, but the (undisclosed) number of sales was "the lowest ever recorded for a May month [for Xbox hardware]", which certainly does.
+    
+    Sadly, it seems these numbers are unlikely to increase any time soon as the price of consoles keeps going up. Just yesterday, Microsoft raised the price of Xboxes again and by a not-inconsiderable amount. Nintendo is also raising the price of Switch 2 later this year and Sony increased the price of PlayStation in March.
+    
+    Circana's report also said the average price paid for a new unit of video game hardware rose to $502 USD in May 2026, whereas it was $440 last year. PS5 pricing specifically rose 33 percent to $672, and Xbox Series X/S pricing climbed 22 percent to $524. And this in a world where the increased cost of living is squeezing away whatever spare income people have.
+    
+    Perhaps more worryingly, the predominant reason for the price increases - the component shortage caused by insatiable AI datacentre demand - shows no sign of abating. Microsoft yesterday said it expects "another doubling" in component pricing by autumn 2027, which presumably means one thing: more price hikes. Or perhaps exorbitantly priced next-generation machines, if any company is bold enough to introduce them in this economic situation.
+    
+    Look at Valve's Steam Machine: its console-like performance at a base price of $1000 has tainted its arrival. Even Valve admitted the Steam Machine price was "significantly" higher than it imagined it would be when designing it. We're living in unprecedented times; never before in a console generation have prices increased like this after launch.
+    
+    The question is whether Grand Theft Auto 6 has the pulling power to reivigorate the console market when it arrives 19th November. One retailer has already warned that there may not be enough console stock to satisfy GTA 6 demand this Christmas or holiday season.</p>
+</section>
+
+</div>
+
+
+
+<?php get_footer();?>
